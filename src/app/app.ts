7ba592e824886362acb2bb4448fb7aac1038eb05
María Ángeles Berbeira Torres <mbt24gr027@student.esgerencia.com>
2025-11-26 10:53:52 +0100

    Componente nuevo visible

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NuevoComponenteVisible } from './nuevo-componente-visible/nuevo-componente-visible';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NuevoComponenteVisible],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
