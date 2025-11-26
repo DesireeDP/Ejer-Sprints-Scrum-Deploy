7b9a4b257ebf6e941d8947d5a8144e9d3075c44e
María Ángeles Berbeira Torres <mbt24gr027@student.esgerencia.com>
2025-11-26 11:06:35 +0100

    Componente visible 2

---
+++ src/app/componente-visible2/componente-visible2.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ComponenteVisible2 } from './componente-visible2';
+
+describe('ComponenteVisible2', () => {
+  let component: ComponenteVisible2;
+  let fixture: ComponentFixture<ComponenteVisible2>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ComponenteVisible2]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ComponenteVisible2);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
